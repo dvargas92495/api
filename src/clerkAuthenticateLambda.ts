690d0a7a9d30fba58d503a2a3972e13fad7a3f5d
David Vargas <dvargas92495@gmail.com>
2021-11-01 18:01:18 -0400

    allow anonymous option in clerk authenticate lambda

--- src/clerkAuthenticateLambda.ts
+++ src/clerkAuthenticateLambda.ts
@@ -6,53 +6,69 @@ const headers = {
 };
 
 const authenticate =
-  (fcn: APIGatewayProxyHandler): APIGatewayProxyHandler =>
+  (
+    fcn: APIGatewayProxyHandler,
+    options: { allowAnonymous?: true } = {}
+  ): APIGatewayProxyHandler =>
   (event, context, callback) => {
-    const sessionToken = (
-      event.headers.Authorization ||
-      event.headers.authorization ||
-      ""
-    ).replace(/^Bearer /, "");
-    if (!sessionToken) {
-      return Promise.resolve({
-        statusCode: 401,
-        body: "No session token found",
-        headers,
-      });
-    }
-    return clerk
-      .verifyToken(sessionToken)
-      .then((session) => (session.sub ? users.getUser(session.sub) : undefined))
-      .then((user) => {
-        if (user) {
-          if (!user.id)
-            return Promise.resolve({
-              statusCode: 401,
-              body: "Invalid User",
-              headers,
-            });
-          const response = fcn(
-            {
-              ...event,
-              requestContext: { ...event.requestContext, authorizer: { user } },
-            },
-            context,
-            callback
-          );
-          return (
-            response ||
-            Promise.resolve({
-              statusCode: 204,
-              body: "",
-              headers,
-            })
-          );
-        }
-        return Promise.resolve({
-          statusCode: 401,
-          body: "No user found",
+    const execute = (user: unknown) => {
+      const response = fcn(
+        {
+          ...event,
+          requestContext: { ...event.requestContext, authorizer: { user } },
+        },
+        context,
+        callback
+      );
+      return (
+        response || {
+          statusCode: 204,
+          body: "",
           headers,
-        });
+        }
+      );
+    };
+    return Promise.resolve()
+      .then(() => {
+        const sessionToken = (
+          event.headers.Authorization ||
+          event.headers.authorization ||
+          ""
+        ).replace(/^Bearer /, "");
+        if (!sessionToken) {
+          return {
+            statusCode: 401,
+            body: "No session token found",
+            headers,
+          };
+        }
+        return clerk
+          .verifyToken(sessionToken)
+          .then((session) =>
+            session.sub ? users.getUser(session.sub) : undefined
+          )
+          .then((user) => {
+            if (user) {
+              if (!user.id)
+                return {
+                  statusCode: 401,
+                  body: "Invalid User",
+                  headers,
+                };
+              return execute(user);
+            }
+            return {
+              statusCode: 401,
+              body: "No user found",
+              headers,
+            };
+          });
+      })
+      .then((r) => {
+        if (options.allowAnonymous && r.statusCode === 401) {
+          return execute(undefined);
+        }
+        return r;
       })
       .catch((e) => {
         return {
