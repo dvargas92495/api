3fcd391e8f7ef269ddd19d56254cc47b51ec7139
David Vargas <dvargas92495@gmail.com>
2021-11-26 20:39:55 -0500

    filter for connected connectison

--- src/connectTypeorm.ts
+++ src/connectTypeorm.ts
@@ -7,21 +7,24 @@ import {
 import randomstring from "randomstring";
 
 export const connect = (entities: EntitySchema[]): Promise<Connection> =>
-  Promise.all(getConnectionManager().connections.map((c) => c.close())).then(
-    () =>
-      createConnection({
-        name: randomstring.generate({ charset: "alphabetic", length: 16 }),
-        entities,
-        type: "mysql",
-        migrations: undefined,
-        logging: process.env.TYPEORM_LOGGING === "true",
-        synchronize: false,
-        port: 5432,
-        database: process.env.TYPEORM_DATABASE,
-        password: process.env.TYPEORM_PASSWORD,
-        username: process.env.TYPEORM_USERNAME,
-        host: process.env.TYPEORM_HOST,
-      })
+  Promise.all(
+    getConnectionManager()
+      .connections.filter((c) => c.isConnected)
+      .map((c) => c.close())
+  ).then(() =>
+    createConnection({
+      name: randomstring.generate({ charset: "alphabetic", length: 16 }),
+      entities,
+      type: "mysql",
+      migrations: undefined,
+      logging: process.env.TYPEORM_LOGGING === "true",
+      synchronize: false,
+      port: 5432,
+      database: process.env.TYPEORM_DATABASE,
+      password: process.env.TYPEORM_PASSWORD,
+      username: process.env.TYPEORM_USERNAME,
+      host: process.env.TYPEORM_HOST,
+    })
   );
 
 export default connect;
