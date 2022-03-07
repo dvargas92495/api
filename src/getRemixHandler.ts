aeeef802504e1a7b26ee9edc670048a789e26c5c
David Vargas <dvargas92495@gmail.com>
2022-03-06 22:01:55 -0600

    0.6.0

--- src/getRemixHandler.ts
+++ src/getRemixHandler.ts
@@ -1,20 +1,23 @@
 import { createRequestHandler } from "@dvargas92495/remix-lambda-at-edge";
 // import sendEmail from "aws-sdk-plus/dist/sendEmail";
 
-const getRemixHandler = ({originPaths = []}:{originPaths?:RegExp[]} = {}) => createRequestHandler({
-  build: require("./build"),
-  originPaths: [
-    "favicon.ico",
-    /^\/build\/.*/,
-    /^\/images\/.*/,
-    ...originPaths,
-  ],
-  onError: (e) => console.log("Send email to me", e),
-  /*sendEmail({
+const getRemixHandler = ({
+  originPaths = [],
+}: { originPaths?: RegExp[] } = {}) =>
+  createRequestHandler({
+    build: require("./build"),
+    originPaths: [
+      "favicon.ico",
+      /^\/build\/.*/,
+      /^\/images\/.*/,
+      ...originPaths,
+    ],
+    onError: (e) => console.log("Send email to me", e),
+    /*sendEmail({
       to: "support@constancy.fund",
       subject: "Remix Origin Error",
       body: e.message,
     }),*/
-});
+  });
 
 export default getRemixHandler;
